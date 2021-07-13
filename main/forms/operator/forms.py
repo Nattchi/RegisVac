b564f791fcb75dd12d26b41fa53e0a8e7c9e8d40
Nattchi <Yuu.tan@icloud.com>
2021-07-13 21:34:24 +0900

    操作者登録画面 使用しないメソッドのを削除 ・fix operator/form.py to delete unused method

--- main/forms/operator/forms.py
+++ main/forms/operator/forms.py
@@ -84,34 +84,6 @@ class JPUserCreationForm(UserCreationForm):
 
         return email
 
-    def clean_password2(self):
-        password1 = self.cleaned_data.get("password1")
-        password2 = self.cleaned_data.get("password2")
-        if password1 and password2 and password1 != password2:
-            raise ValidationError(
-                self.error_messages['password_mismatch'],
-                code='password_mismatch',
-            )
-        return password2
-
-    def _post_clean(self):
-        super()._post_clean()
-        # Validate the password after self.instance is updated with form data
-        # by super().
-        password = self.cleaned_data.get('password2')
-        if password:
-            try:
-                password_validation.validate_password(password, self.instance)
-            except ValidationError as error:
-                self.add_error('password1', error)
-
-    def save(self, commit=True):
-        user = super().save(commit=False)
-        user.set_password(self.cleaned_data["password1"])
-        if commit:
-            user.save()
-        return user
-
 
 class MyUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm):
