a873add6393159729639ce298e2f081b37ee7837
Michiaki Hirose <michiaki.hirose.7y@stu.hosei.ac.jp>
2021-07-13 23:28:25 +0900

    ログイン・新規登録・ログアウト画面の改良。

--- RegisVac/settings.py
+++ RegisVac/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'knu6@q#hyr79f%9wv(6hug@lh=wg(f#razynclxcxou7%doaxt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
